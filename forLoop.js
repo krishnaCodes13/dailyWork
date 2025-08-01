1904f828d5a32447464a72a7d46de41eef2dd9c9
Krishna Yadav <yadavkrishnarajendra@gmail.com>
2025-08-01 20:33:30 +0530

    changes

--- forLoop.js
+++
@@ -1,5 +0,0 @@
-let array = ["HTML", "JAVASCRIPT", "CSS", "JAVA", "SPRINGBOOT"];
-
-for(let i=0; i<array.length; i++){
-    console.log(array[i]);
-}
--- whileLoop.js
+++
@@ -1,7 +0,0 @@
-var x = 2;
-var y=1;
-
-while(y<=10){
-    console.log(x+" X "+y+" = "+ x*y);
-    y++;
-}
